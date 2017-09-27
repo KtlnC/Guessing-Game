1bba2402f26659f1a9c8267656e4a39dc8a342d8
BT_1N3_13 <BT_1N3_13@BT_1N3_13.bktech.local>
2017-09-27 13:10:58 -0400

    error occurs when input isn't a number

--- game/src/game/GuessingGame.java
+++ game/src/game/GuessingGame.java
@@ -11,30 +11,32 @@ import java.util.InputMismatchException;
 public class GuessingGame
 {
 	//next line is unnecessary, so is the close statement
-	private static Scanner input;
+//	private static Scanner input;
 	public static void main(String[] args)
 	{
-  		input = new Scanner(System.in);
+  		Scanner input = new Scanner(System.in);
 		System.out.println("What is your name?");
 		String s = input.next();
 		System.out.println("Nice to meet you, " + s);
-		boolean isNumber = false;
 		int answer = (int) (Math.random() * 10 + 1);
 		int guess = 0;
 		//use input.reset() in the inner loop
 		while(guess != answer)
 		{
+			boolean isNumber = false;
 			while(!isNumber)
 			{
 				try
 				{
 					System.out.println("Pick a number from 1 - 10");
+					input.reset();	
 					guess = input.nextInt();
 					isNumber = true;
 				}
 				catch(InputMismatchException e)
 				{
 					System.out.println("That was error type: " + e);
+					
 				}				
 			}
 			if(guess > answer)
